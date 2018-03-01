160c97fcae2a068ba9d1ac61bacaf7efd29e4485
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2018-03-01 00:32:07 -0500

    Added start game button & minor styling improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button, Media, Panel, Badge, Glyphicon, Image, Grid, Row, Col, Navbar, NavItem, NavDropdown, Nav, MenuItem } from 'react-bootstrap';
+import { Button, Media, Panel, Badge, Glyphicon, Image, Grid, Row, Col, Navbar, NavItem, Nav } from 'react-bootstrap';
 
 class App extends Component {
   STARTYEAR = 2013; // copyright start year
@@ -41,6 +41,14 @@ class App extends Component {
           </Navbar>
         </Row>
 
+        <Row style={{ paddingBottom: '1em' }}>
+          <Col xs={12} md={6}>
+            <Button bsSize="large" bsStyle="success" block>Start New Game</Button>
+          </Col>
+          <Col xs={12} md={6}>
+            <Button bsSize="large" block>Select Game Variant</Button>
+          </Col>
+        </Row>
 
         <Row>
           <Col xs={12} md={6}>
@@ -50,40 +58,43 @@ class App extends Component {
 
                 <Media>
                   <Media.Left>
-                    <Image src={logo} width="64" height="64" circle />
+                    <Image src={logo} width="48" height="48" circle />
                   </Media.Left>
                   <Media.Body>
+                    <div className="pull-right" style={{ marginLeft: '1em' }}>
+                      <Button bsSize="small" bsStyle="success">Accept</Button> <Button bsSize="small" bsStyle="danger">Regect</Button>
+                    </div>
                     <Media.Heading>Alkonaft invites you to play</Media.Heading>
                     <p>
                       Game 4 x 6 / 10 total turns
                     </p>
-                    <Button bsSize="small" bsStyle="success">Accept</Button> <Button bsSize="small" bsStyle="danger">Regect</Button>
                   </Media.Body>
                 </Media>
 
                 <Media>
                   <Media.Left>
-                    <Image src={logo} width="64" height="64" circle />
+                    <Image src={logo} width="48" height="48" circle />
                   </Media.Left>
                   <Media.Body>
+                    <Button className="pull-right" style={{ marginLeft: '1em' }} bsSize="small" bsStyle="success">Make Your Move</Button>
                     <Media.Heading>Alkonaft vs. Sergey Chernyshev</Media.Heading>
                     <p>
                       Turn 4.1: You turn
                     </p>
-                    <Button bsSize="small" bsStyle="success">Make your move</Button>
+
                   </Media.Body>
                 </Media>
 
                 <Media>
                   <Media.Left>
-                    <Image src={logo} width="64" height="64" circle />
+                    <Image src={logo} width="48" height="48" circle />
                   </Media.Left>
                   <Media.Body>
-                    <Media.Heading>Alkonaft invites you to play</Media.Heading>
+                    <Button className="pull-right" style={{ marginLeft: '1em' }} bsSize="small" bsStyle="info">View Game</Button>
+                    <Media.Heading>Sergey Chernyshev vs. Alkonaft</Media.Heading>
                     <p>
-                      Game 4 x 6 / 10 total turns
+                      Congratulations, you won!
                     </p>
-                    <Button bsSize="small" bsStyle="success">Accept</Button> <Button bsSize="small" bsStyle="danger">Regect</Button>
                   </Media.Body>
                 </Media>
 
@@ -106,9 +117,9 @@ class App extends Component {
           </Col>
         </Row>
         <Row>
-          <footer class={this.inverse ? 'footer navbar navbar-inverse' : 'footer navbar navbar-default'}>
-            <div class="container">
-              <ul class="nav navbar-nav pull-left">
+          <footer className={this.inverse ? 'footer navbar navbar-inverse' : 'footer navbar navbar-default'}>
+            <div className="container">
+              <ul className="nav navbar-nav pull-left">
                 <li>
                   <a href="/">
                     {this.STARTYEAR}-{this.YEAR} &copy; Shareholder Game
