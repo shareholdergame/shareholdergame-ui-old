ef747488a5b69f5d02f8ddeaabdf7aa7b05b0e31
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2018-03-04 10:34:06 -0500

    Fixed a couple eslint issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
