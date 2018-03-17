68a4ee52bbc146cddf4ff23eab5d0c7041d9c350
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2018-03-17 00:24:26 -0400

    Some formatting improvements on my games page

--- src/MyGames/GameTile.jsx
+++ src/MyGames/GameTile.jsx
@@ -1,43 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Image, Well } from "react-bootstrap";
-import { FormattedMessage } from "react-intl";
+import { Row, Col, Glyphicon, Button, Image, Well } from "react-bootstrap";
+import { FormattedDate, FormattedTime, FormattedMessage } from "react-intl";
 import { arrayOf, shape, number, string } from "prop-types";
+import { LinkContainer } from "react-router-bootstrap";
 
 const gameLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 const GameTile = props => (
   <Well>
-    <span className="pull-right">
-      <FormattedMessage
-        id="myprops.games.game.started"
-        description="props.game started time text on props.game tile"
-        defaultMessage="Started: {started}"
-        values={{
-          started: props.game.started
-        }}
-      />
-    </span>
-    <FormattedMessage
-      id="myprops.games.game.type"
-      description="props.game type text on props.game tile"
-      defaultMessage="{major} x {minor} - {total} turns"
-      values={{
-        major: props.game.options.major,
-        minor: props.game.options.minor,
-        total: props.game.options.major + props.game.options.minor
-      }}
-    />
+    <Row>
+      <Col md="6">
+        <FormattedMessage
+          id="mygames.game.type"
+          description="props.game type text on props.game tile"
+          defaultMessage="{major} x {minor} - {total} turns"
+          values={{
+            major: props.game.options.major,
+            minor: props.game.options.minor,
+            total: props.game.options.major + props.game.options.minor
+          }}
+        />
+      </Col>
+      <Col md="6">
+        <FormattedMessage
+          id="mygames.game.started"
+          description="props.game started time text on props.game tile"
+          defaultMessage="Started:"
+        />{" "}
+        <FormattedDate
+          value={new Date(props.game.started)}
+          year="numeric"
+          month="short"
+          day="2-digit"
+        />{" "}
+        @ <FormattedTime value={new Date(props.game.started)} />,{" "}
+      </Col>
+    </Row>
 
-    <div>
+    <p style={{ fontSize: "x-large", textAlign: "center" }}>
       {props.game.players
         .map(player => (
           <span>
             <Link to={`/players/${player.name}`}>
               <Image
                 src={`/images/userpics/${player.userpic}`}
-                width="16"
-                height="16"
+                width="38"
+                height="38"
                 circle
               />
             </Link>{" "}
@@ -50,7 +59,7 @@ const GameTile = props => (
           if (props.game.players.length > players.length) {
             players.push(
               <FormattedMessage
-                id="myprops.games.game.vs"
+                id="mygames.game.vs"
                 description="Versus separator between player names props.game tile"
                 defaultMessage=" - "
               />
@@ -59,16 +68,36 @@ const GameTile = props => (
 
           return players;
         }, [])}
-    </div>
+    </p>
 
-    <div>
-      <span>Game {gameLetters[props.game.turn]}</span>{" "}
-      <span>
-        Move {props.game.round}.{props.game.turn}
-      </span>{" "}
-      <span>
-        Move {props.game.round}.{props.game.turn}
+    <p style={{ fontSize: "x-large", textAlign: "center" }}>
+      <span style={{ marginRight: "2em" }}>
+        Game {gameLetters[props.game.turn - 1]}
       </span>
+      <span>
+        <FormattedMessage
+          id="mygames.gametile.turn"
+          description="Turn number on game tile"
+          defaultMessage="Move {round}.{turn}"
+          values={{
+            round: props.game.round,
+            turn: props.game.turn
+          }}
+        />
+      </span>{" "}
+    </p>
+
+    <div style={{ textAlign: "center" }}>
+      <LinkContainer to={`/game/${props.game.id}`}>
+        <Button bsStyle="success" bsSize="large">
+          <FormattedMessage
+            id="global.yourturn.button"
+            description="Button label text for your turn call to action"
+            defaultMessage="Make Your Move"
+          />{" "}
+          <Glyphicon glyph="log-in" />
+        </Button>
+      </LinkContainer>
     </div>
   </Well>
 );
