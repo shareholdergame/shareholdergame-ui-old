136a8867426acbc5871822646d2d191389efa8b3
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2018-11-07 22:57:53 -0500

    Rendered small card with bottom section

--- src/Cards/SmallCard.jsx
+++ src/Cards/SmallCard.jsx
@@ -9,6 +9,11 @@ class SmallCard extends Card {
 
     this.value = value;
     this.cardString = this.value > 0 ? `+${this.value}` : this.value;
+
+    this.oppositeValue = this.value > 0 ? this.value - 70 : 70 + this.value;
+    this.oppositeString =
+      this.oppositeValue > 0 ? `+${this.oppositeValue}` : this.oppositeValue;
+
     this.cardLabel = <SmallCardLabel card={this} />;
   }
 
--- src/Cards/SmallCardLabel.jsx
+++ src/Cards/SmallCardLabel.jsx
@@ -2,14 +2,68 @@ import React from "react";
 import { number, shape, string, node } from "prop-types";
 import { injectIntl, intlShape } from "react-intl";
 
-import CardLabel from "./CardLabel";
+import Color from "color";
 
-const SmallCardLabel = ({ card, intl }) => (
-  <CardLabel color={card.color.style}>
-    {card.cardString}
-    {intl.formatMessage(card.color.letter)}
-  </CardLabel>
-);
+const SmallCardLabel = ({ card, intl }) => {
+  const style = {
+    backgroundColor: "white",
+    padding: "0 0.5em",
+    margin: "0 0.5em",
+    border: `1px solid silver`,
+    display: "block",
+    textAlign: "center"
+  };
+
+  const upper = {
+    borderTopLeftRadius: "0.5em",
+    borderTopRightRadius: "0.5em",
+    borderBottom: "1px solid silver"
+  };
+
+  const lower = {
+    borderBottomLeftRadius: "0.5em",
+    borderBottomRightRadius: "0.5em",
+    borderTop: "0"
+  };
+
+  const cardStyle = {
+    backgroundColor: Color(card.color.style).alpha(0.2),
+    border: `1px solid ${Color(card.color.style).darken(0.3)}`
+  };
+
+  const upperLabel =
+    card.value > 0
+      ? `${card.cardString}${intl.formatMessage(card.color.letter)}`
+      : `${card.oppositeString}?`;
+
+  const lowerLabel =
+    card.value < 0
+      ? `${card.cardString}${intl.formatMessage(card.color.letter)}`
+      : `${card.oppositeString}??`;
+
+  return (
+    <span style={{ display: "inline-block" }}>
+      <span
+        style={
+          card.value > 0
+            ? { ...style, ...cardStyle, ...upper }
+            : { ...style, ...upper }
+        }
+      >
+        {upperLabel}
+      </span>
+      <span
+        style={
+          card.value < 0
+            ? { ...style, ...cardStyle, ...lower }
+            : { ...style, ...lower }
+        }
+      >
+        {lowerLabel}
+      </span>
+    </span>
+  );
+};
 
 SmallCardLabel.propTypes = {
   card: shape({
